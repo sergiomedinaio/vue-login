f16d8b70061eb44ad42d7640d6e6e4ba498c9f53
Sergio Medina <hola@sergiomedina.io>
2022-11-16 15:34:42 -0300

    style: update theme colors

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -4,4 +4,17 @@ import Vuetify from 'vuetify/lib/framework';
 Vue.use(Vuetify);
 
 export default new Vuetify({
+    theme: {
+        themes: {
+            light:   {
+                primary: '#9c27b0',
+                secondary: '#795548',
+                accent: '#cddc39',
+                error: '#f44336',
+                warning: '#ff9800',
+                info: '#03a9f4',
+                success: '#4caf50'
+            }
+        }
+    }
 });
