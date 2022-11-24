72e344ea1b6f4740c179998bbcbcb41bde407dc3
Sergio Medina <hola@sergiomedina.io>
2022-11-24 15:20:09 -0300

    feat: add password setup

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,40 +2,15 @@
   <v-container>
     <v-row>
       <v-col>
-        <v-alert
-          :value="alert"
-          color="pink"
-          dark
-          border="top"
-          icon="mdi-home"
-          transition="scale-transition"
-        >
-          Phasellus tempus. Fusce ac felis sit amet ligula pharetra condimentum. In dui magna, posuere eget, vestibulum et, tempor auctor, justo. Pellentesque posuere. Curabitur ligula sapien, tincidunt non, euismod vitae, posuere imperdiet, leo.
-    
-          Phasellus nec sem in justo pellentesque facilisis. Phasellus magna. Cras risus ipsum, faucibus ut, ullamcorper id, varius ac, leo. In hac habitasse platea dictumst. Praesent turpis.
-        </v-alert>
-      </v-col>
-    </v-row>
-    <v-row>
-      <v-col>
-        <v-card
-          class="mx-auto"
-          max-width="400"
-        >
-          <v-img
-            class="white--text align-end"
-                contain
-                height="100"
-      
-            :src="require('../assets/logo.svg')"
-          >
+        <v-card class="mx-auto" max-width="400">
+          <v-img class="white--text align-end" contain height="100" :src="require('../assets/logo.svg')">
           </v-img>
           <v-card-text>
             <v-form>
               <v-row justify="center">
                 <v-col cols="12" sm="6" md="10">
-                  <v-text-field label="Email" placeholder="Email"></v-text-field>
-                  <v-text-field label="Password" placeholder="Password"></v-text-field>
+                  <v-text-field v-model="email" label="Email" placeholder="Email"></v-text-field>
+                  <v-text-field v-model="password" label="Password" placeholder="Password"></v-text-field>
                 </v-col>
               </v-row>
             </v-form>
@@ -43,18 +18,11 @@
           <v-card-actions>
             <v-row justify="center">
               <v-col>
-                <v-btn
-                  color="orange"
-                  text
-                  @click="alert = !alert"
-                >
+                <v-btn color="orange" text @click="validarInformacion">
                   Iniciar sesión
                 </v-btn>
-          
-                <v-btn
-                  color="orange"
-                  text
-                >
+
+                <v-btn color="orange" text>
                   Registrarse
                 </v-btn>
 
@@ -64,68 +32,55 @@
         </v-card>
       </v-col>
       <template>
-</template>    </v-row>
-    <v-row class="text-center">
+      </template>
+    </v-row>
+    <v-row>
+      <v-col>
+        <v-alert :value="alert" color="pink" dark border="top" icon="mdi-home" transition="scale-transition">
+            {{mensajeAlert}}
+        </v-alert>
+        <v-alert :value="success" color="success" dark border="top" icon="mdi-home" transition="scale-transition">
+            Se ingresó correctamente sesión
+        </v-alert>
+      </v-col>
     </v-row>
   </v-container>
 </template>
 
 <script>
-  export default {
-    name: 'HelloWorld',
-
-    data: () => ({
+export default {
+  name: 'HelloWorld',
+  data: () => {
+    return {
+      email: "",
+      password: "",
+      success: false,
       alert: false,
-      ecosystem: [
-        {
-          text: 'vuetify-loader',
-          href: 'https://github.com/vuetifyjs/vuetify-loader',
-        },
-        {
-          text: 'github',
-          href: 'https://github.com/vuetifyjs/vuetify',
-        },
-        {
-          text: 'awesome-vuetify',
-          href: 'https://github.com/vuetifyjs/awesome-vuetify',
-        },
-      ],
-      importantLinks: [
-        {
-          text: 'Documentation',
-          href: 'https://vuetifyjs.com',
-        },
-        {
-          text: 'Chat',
-          href: 'https://community.vuetifyjs.com',
-        },
-        {
-          text: 'Made with Vuetify',
-          href: 'https://madewithvuejs.com/vuetify',
-        },
-        {
-          text: 'Twitter',
-          href: 'https://twitter.com/vuetifyjs',
-        },
-        {
-          text: 'Articles',
-          href: 'https://medium.com/vuetify',
-        },
-      ],
-      whatsNext: [
-        {
-          text: 'Explore components',
-          href: 'https://vuetifyjs.com/components/api-explorer',
-        },
-        {
-          text: 'Select a layout',
-          href: 'https://vuetifyjs.com/getting-started/pre-made-layouts',
-        },
-        {
-          text: 'Frequently Asked Questions',
-          href: 'https://vuetifyjs.com/getting-started/frequently-asked-questions',
-        },
-      ],
-    }),
+      mensajeAlert: ""
+    }
+  },
+  methods: {
+    validarInformacion(e){
+      this.alert = false;
+      this.success = false;
+      this.mensajeAlert = "";
+      console.log(e)
+      if(!this.email) { // si esta vacio
+        this.mensajeAlert += "El email es obligatorio.";
+        this.alert = true;
+      }
+      if(!this.password) {
+        this.mensajeAlert += "El password es obligatorio.";
+        this.alert = true;
+      }
+      if(!this.mensajeAlert) {
+        this.success = true;
+      }
+      setTimeout(()=> {
+        this.success = false;
+        this.alert = false;
+      }, 2000);
+    }
   }
+}
 </script>
