131fa9a08a3020cc2be9cca9a132e253a132010d
Sergio Medina <63879459+sergiomedinaio@users.noreply.github.com>
2022-12-01 20:10:54 +0000

    feat: add signout method

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -6,6 +6,33 @@
   </template>
 
   <script>
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "firebase/app";
+  import { getAuth, signOut  } from "firebase/auth";
+
+  //import { getAnalytics } from "firebase/analytics";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyCU5Zt6ZpfSdmiEJxrvWztBDGpOtK8zvGY",
+    authDomain: "vue-login-27f87.firebaseapp.com",
+    projectId: "vue-login-27f87",
+    storageBucket: "vue-login-27f87.appspot.com",
+    messagingSenderId: "75332090700",
+    appId: "1:75332090700:web:4d56b217ac78a6043aede9",
+    measurementId: "G-2DVW4S5SHT"
+  };
+
+  // Initialize Firebase
+  const firebaseApp = initializeApp(firebaseConfig);
+  console.log(firebaseApp);
+  const auth = getAuth(firebaseApp);
+
+  //const analytics = getAnalytics(app);
+
 
   export default {
     name: 'about-view',
@@ -22,9 +49,15 @@
     },
     methods: {
       logout: function() {
-          console.log("cerrar sesion");
-          localStorage.setItem("login", JSON.stringify(false));
-          this.$router.push("/login");  
+        signOut(auth)
+          .then(() => {
+            console.log("cerrar sesion");
+            localStorage.setItem("login", JSON.stringify(false));
+            this.$router.push("/login");  
+          })
+          .catch(error => {
+            console.error(error);
+          });
       }
     }
   }
