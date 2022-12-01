083ac7ec7125972a294e7ef62e6f387ccc85970a
Sergio Medina <63879459+sergiomedinaio@users.noreply.github.com>
2022-12-01 19:43:50 +0000

    feat: add firebase support

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -48,6 +48,30 @@
 </template>
 
 <script>
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+//import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCU5Zt6ZpfSdmiEJxrvWztBDGpOtK8zvGY",
+  authDomain: "vue-login-27f87.firebaseapp.com",
+  projectId: "vue-login-27f87",
+  storageBucket: "vue-login-27f87.appspot.com",
+  messagingSenderId: "75332090700",
+  appId: "1:75332090700:web:4d56b217ac78a6043aede9",
+  measurementId: "G-2DVW4S5SHT"
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+console.log(firebaseApp);
+//const analytics = getAnalytics(app);
+
 export default {
   name: 'HelloWorld',
   data: () => {
